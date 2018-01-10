06c6cbc8731966edc72de8d295c0ce470646b068
Bruno <bruno.torres.fabila@gmail.com>
2018-01-10 16:05:53 -0600

    Finish queue & stack functinoal implementations

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,44 @@
+/* Queue is constructor function
+     define storage, count, and lowestCount
+   Create enqueue method, accepts values
+     adds value to queue
+     count increases by 1
+   Create dequeue method
+     Save last value to storage
+     delete last value
+     lowest count increases by 1
+     return saved value
+   Create size method
+     returns count minus lowestCount
+  */
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var valueIn = 0;
+  var valueOut = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (value) {
+      storage[valueIn] = value;
+      valueIn++;
+    }
   };
 
   someInstance.dequeue = function() {
+    if (valueIn - valueOut === 0) {
+      return undefined;
+    }
+    let answer = storage[valueOut];
+    delete storage[valueOut];
+    valueOut++;
+    return answer;
   };
 
   someInstance.size = function() {
+    return valueIn - valueOut;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,43 @@
+/*  Stack is constructor function
+      create count = 0, create storage object
+    define Push method
+      adds value into storage, count++
+    define Pop method
+      Check if stack is empty
+        if true, return undefined
+      count--, save top element to variable
+      delete top element from storage
+      return saved element
+    define Size method
+      returns count
+*/
+
 var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  let count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    if (value) {
+      storage[count] = value;
+      count++;
+    }
   };
 
   someInstance.pop = function() {
+    if (count === 0) {
+      return undefined;
+    }
+    count--;
+    var topElm = storage[count];
+    delete storage[count];
+    return topElm;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
