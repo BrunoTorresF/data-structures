96fe5fc7cd567ddec75902a6e1dbf46a9fc61109
Bruno <bruno.torres.fabila@gmail.com>
2018-01-12 16:15:30 -0600

    Finish linked list implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,51 @@
+/*New linked list only has head, tail; both null
+  Add elements toTail: tail points to element, last element points to new elements
+    if list is empty, list.head = new node; list.tail = head
+    else, last node (aka this.tail) next pointer = new node
+    tail pointer switches to created node
+  Removing head, delete element, head points to next element
+    Don't explicitly delete element, only change pointer
+  Contains = traverse through list, from head to tail, applying truth test
+    is list.head equal to target, if not return false
+    if yes, return true
+    continue if this.tail.next is not null
+*/
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    if (!this.head) {
+      return undefined;
+    } else {
+      let oldHead = this.head.value;
+      this.head = this.head.next
+      return oldHead;
+    }
   };
 
   list.contains = function(target) {
+    let testNode = this.head;
+    while (testNode) {
+      if (testNode.value === target) {
+        return true;
+      }
+      testNode = testNode.next;
+    }
+    return false;
   };
 
   return list;
