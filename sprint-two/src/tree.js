0a216c9229d539d6879072fee12efc56e39e530c
Bruno <bruno.torres.fabila@gmail.com>
2018-01-12 19:33:01 -0600

    Finish writing tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,35 @@
+/*tree is a hierarchical recursive structure
+  parent node has multiple child nodes; first node is root
+
+  */
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  newTree.parent = null;
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods)
 
   return newTree;
 };
 
-var treeMethods = {};
-
+var treeMethods = {}
 treeMethods.addChild = function(value) {
+  let child = Tree(value);
+  child.parent = this;
+  this.children.push(child);
 };
 
-treeMethods.contains = function(target) {
-};
+ treeMethods.contains = function(target) {
+   if (this.value === target) {
+     return true;
+   }
+   return _.some(this.children, function(child) {
+     return child.contains(target)
+   })
+  }
 
-
-
+/*
 /*
  * Complexity: What is the time complexity of the above functions?
  */
