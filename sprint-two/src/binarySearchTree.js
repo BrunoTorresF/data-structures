22815a83baa6b057500d9257890bf48ba8b100fb
BrunoTorresF <bruno.torres.fabila@gmail.com>
2018-05-23 19:07:09 -0500

    Begin BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,38 @@
+
 var BinarySearchTree = function(value) {
+  var newBinarySearchTree = Object.create(BinarySearchTree.prototype);
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+  newBinarySearchTree.value = value;
+  return newBinarySearchTree;
 };
 
+  BinarySearchTree.prototype.insert = function (value) {
+    var newBST = BinarySearchTree(value);
 
+    var add = function(child, node) {
+      if (child.value < node.value && node.left === null) {
+        node.left = child;
+      } else {
+        return add(child, node.left);
+      }
+      if (child.value > node.value && node.value === null) {
+        node.right = child;
+      } else {
+        return add(child, node.right);
+      }
+    }
+
+    add(newBST, this);
+  };
+
+  BinarySearchTree.prototype.contains = function (value) {
+
+  };
+
+  BinarySearchTree.prototype.depthFirstLog = function (cb) {
+
+  };
 /*
  * Complexity: What is the time complexity of the above functions?
  */
